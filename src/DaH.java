e7cfff7b0ebed0a3e8f41e538ab8d8bd8c918696
Jakub Tomczuk <jakub.tmczk@gmail.com>
2016-05-28 15:50:52 +0200

    Przepelnione inty

---
+++ src/DaH.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+/**
+ * Created by Jakub Tomczuk on 28.05.2016.
+ */
+public class DaH /*Deffie and Hellman*/{
+    int q = 19;
+    int a = 2; //tutaj albo z palca albo tablica albo metoda?
+    Random generator = new Random();
+    private int alice = generator.nextInt(q-1) + 1;//Mi
+    private int bob = generator.nextInt(q-1) + 1;//Mj
+    /* int alice,bob = 4 */
+
+
+    public static void main(String[] args) {
+        DaH program = new DaH();
+        program.run();
+    }
+
+    private void run(){
+        System.out.println("Alice secret number: " + alice + "\nBob secret number: " + bob);
+
+        int alice_calculate = calculateC(alice,a,q);
+        int bob_calculate = calculateC(bob,a,q);
+        System.out.println("Public file numbers: " + alice_calculate +" " + bob_calculate);
+
+        int alice_public = commonKey(bob_calculate,alice,q);
+        int bob_public = commonKey(alice_calculate,bob,q);
+
+        System.out.println("Public keys (should be same)\nAlice: " + alice_public + "\nBob: " + bob_public);
+    }
+
+    private int calculateC(int M, int a, int q){
+
+        int c = ((int) Math.pow(a,M))%q;
+        return c;
+    }
+
+    private int commonKey(int C, int M, int q){
+        int k = 0;
+        k = ((int) Math.pow(C,M))%q;
+        return k;
+    }
+}
